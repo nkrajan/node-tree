75e1e0a85ba48200d2cdc7838ab747974847f835
Laurie <github@seldo.com>
2012-10-26 23:30:46 +0000

    Creating shares and establishing parent-child relationships

--- app.js
+++ app.js
@@ -3,43 +3,54 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+	, routes = require('./routes')
+	, share = require('./routes/share')
+	, http = require('http')
+	, path = require('path');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3001);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.set('port', process.env.PORT || 3001);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 app.get('/about', routes.about);
-app.get('/users', user.list);
+app.get('/shares', share.find);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+	console.log("Express server listening on port " + app.get('port'));
 });
 
 var io = require('socket.io').listen(server);
+var Share = require('./models/share');
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('chatmessage', function (data) {
-    console.log(data);
-	socket.broadcast.emit('broadcast',data);
-  });
+	socket.emit('news', {
+		hello: 'world'
+	});
+	socket.on('createshare', function (data) {
+		console.log("Creating share")
+		console.log(data);
+		Share.create(data,function(err,share){
+			if (err) {
+				console.log("Error creating share")
+				console.log(err)
+			} else {
+				socket.broadcast.emit('sharecreated',share);
+			}
+		});		
+	});
 });
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res) {
-  res.render('index', { title: 'Hello, world!' });
+  res.render('index', { title: 'Welcome!' });
 };
 
 exports.about = function(req, res) {
